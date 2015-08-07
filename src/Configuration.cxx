0de3dbe88a6c57a4e8073469b5a39f51b31ef889
bvonhall <barthelemy.von.haller@cern.ch>
2015-08-07 12:45:42 +0200

    added back my changes for the upper case class

--- src/Configuration.cxx
+++ src/Configuration.cxx
@@ -12,35 +12,41 @@
 
 using namespace std;
 
-class configFilePrivate {
+class ConfigFilePrivate
+{
   public:
-  configFilePrivate();
-  ~configFilePrivate();
-  
-  friend class configFile;
+    ConfigFilePrivate();
+    ~ConfigFilePrivate();
+
+    friend class ConfigFile;
+
   protected:
-    boost::property_tree::ptree pt;    
-    
+    boost::property_tree::ptree pt;
+
 };
 
-configFilePrivate::configFilePrivate(){
+ConfigFilePrivate::ConfigFilePrivate()
+{
 }
 
-configFilePrivate::~configFilePrivate(){
+ConfigFilePrivate::~ConfigFilePrivate()
+{
 }
 
 
-configFile::configFile(){
-  dPtr=new configFilePrivate();
-  if (dPtr==NULL) {
+ConfigFile::ConfigFile()
+{
+  dPtr = new ConfigFilePrivate();
+  if (dPtr == NULL) {
     std::stringstream ss;
     ss << "new() error in " << __FILE__ << " line " << __LINE__;
     throw ss.str();
   }
 }
 
-configFile::~configFile(){
-  if (dPtr!=NULL) {
+ConfigFile::~ConfigFile()
+{
+  if (dPtr != NULL) {
     delete dPtr;
   }
 }
@@ -48,40 +54,41 @@ configFile::~configFile(){
 #define PREFIX_FILE "file:"
 #define SUFFIX_FILE_INI {".ini", ".cfg"}
 
-void configFile::load(const std::string path) {
-  if (path.length()==0) throw std::string("Invalid argument");
+void ConfigFile::load(const std::string path)
+{
+  if (path.length() == 0) { throw std::string("Invalid argument"); }
 
   //
   // open location according to prefix
   //
-  
-  
+
+
   // filesystem file
   if (boost::algorithm::starts_with(path.c_str(), PREFIX_FILE)) {
     const char *filename;
-    filename=&path[strlen(PREFIX_FILE)];
-    
+    filename = &path[strlen(PREFIX_FILE)];
+
     // TODO: filter out comments in file with boost filtering_stream
-    
+
     //
     // parse file according to suffix
     //
-    
+
     // INI file
-    std::vector <const char*>suffix_ini SUFFIX_FILE_INI;
-    for ( auto suffix : suffix_ini ) {
+    std::vector<const char*> suffix_ini SUFFIX_FILE_INI;
+    for (auto suffix : suffix_ini) {
       if (boost::algorithm::ends_with(filename, suffix)) {
         try {
           boost::property_tree::ini_parser::read_ini(filename, dPtr->pt);
         }
-        catch (boost::property_tree::ini_parser::ini_parser_error perr) {    
+        catch (boost::property_tree::ini_parser::ini_parser_error perr) {
           std::stringstream ss;
           if (perr.line()) {
             ss << perr.message() << " in " << perr.filename() << " line " << perr.line();
           } else {
             ss << perr.message() << " " << perr.filename();
           }
-          throw ss.str();    
+          throw ss.str();
         }
         return;
       }
@@ -91,30 +98,33 @@ void configFile::load(const std::string path) {
     throw std::string("Invalid path prefix");
   }
 }
-  
-void configFile::getValue(const std::string key,std::string &value){
+
+void ConfigFile::getValue(const std::string key, std::string &value)
+{
   try {
-    value=dPtr->pt.get<std::string>(key);
+    value = dPtr->pt.get<std::string>(key);
   }
-  catch(const boost::property_tree::ptree_error &e) {
+  catch (const boost::property_tree::ptree_error &e) {
     throw std::string(e.what());
   }
 }
 
-void configFile::getValue(const std::string key, int &value){
+void ConfigFile::getValue(const std::string key, int &value)
+{
   try {
-    value=dPtr->pt.get<int>(key);
+    value = dPtr->pt.get<int>(key);
   }
-  catch(const boost::property_tree::ptree_error &e) {
+  catch (const boost::property_tree::ptree_error &e) {
     throw std::string(e.what());
   }
 }
 
-void configFile::getValue(const std::string key, float &value){
+void ConfigFile::getValue(const std::string key, float &value)
+{
   try {
-    value=dPtr->pt.get<float>(key);
+    value = dPtr->pt.get<float>(key);
   }
-  catch(const boost::property_tree::ptree_error &e) {
+  catch (const boost::property_tree::ptree_error &e) {
     throw std::string(e.what());
   }
 }
