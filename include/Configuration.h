62bf6481e5667de73ad7179ff425cb89912c076e
chapelan <sylvain.chapeland@cern.ch>
2015-08-07 12:29:05 +0200

    replaced char* by std::string

--- include/Configuration.h
+++ include/Configuration.h
@@ -19,26 +19,26 @@ public:
   ///              Accepted suffix, to define file format (TODO: separate doc for file formats):
   ///                 .ini, .cfg    see example.cfg
   /// \exception   Throws a <std::string> exception on error.
-  void load(const char *path);
+  void load(const std::string path);
 
 
   /// Get  the configuration from given path
   /// \param key   key name (possibly hierarchical)
   /// \param value Result (std::string) value found, by reference (this variable is modified in case of success)
   /// \exception   Throws a <std::string> exception on error.
-  void getValue(const char *key, std::string &value); 
+  void getValue(const std::string key, std::string &value); 
   
   /// Get  the configuration from given path
   /// \param key   key name (possibly hierarchical)
   /// \param value Result (int) value found, by reference (this variable is modified in case of success)
   /// \exception   Throws a <std::string> exception on error.
-  void getValue(const char *key, int &value);
+  void getValue(const std::string key, int &value);
   
   /// Get  the configuration from given path
   /// \param key   key name (possibly hierarchical)
   /// \param value Result (float) value found, by reference (this variable is modified in case of success)
   /// \exception   Throws a <std::string> exception on error.
-  void getValue(const char *key, float &value);
+  void getValue(const std::string key, float &value);
 
 private:
     ConfigFilePrivate *dPtr;
