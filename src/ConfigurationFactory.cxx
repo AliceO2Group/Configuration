d3f27d13c3c8d2743d2889cc6184bb7fbd2fcf0e
PascalBoeschoten <passiepascal@gmail.com>
2016-09-28 14:08:30 +0200

    Configuration: Getters now return a boost::optional. Added AliceO2::Configuration namespace.

--- src/ConfigurationFactory.cxx
+++ src/ConfigurationFactory.cxx
@@ -16,6 +16,11 @@ namespace AliceO2
 namespace Configuration
 {
 
+namespace AliceO2
+{
+namespace Configuration
+{
+
 std::unique_ptr<ConfigurationInterface> ConfigurationFactory::getConfiguration(std::string uri)
 {
   http::url parsed = http::ParseHttpUrl(uri);
