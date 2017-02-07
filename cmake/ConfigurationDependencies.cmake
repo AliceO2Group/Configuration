a284d75534a6569e6f556588cf25f3706db5a760
bvonhall <barthelemy.von.haller@cern.ch>
2017-02-07 16:04:39 +0100

    Protobuf is not required

--- cmake/ConfigurationDependencies.cmake
+++ cmake/ConfigurationDependencies.cmake
@@ -5,7 +5,7 @@ find_package(Boost 1.42.0 COMPONENTS unit_test_framework program_options REQUIRE
 find_package(Git QUIET) # if we don't find git or FindGit.cmake is not on the system we ignore it.
 find_package(RapidJSON)
 find_package(CURL REQUIRED)
-find_package(Protobuf REQUIRED 3.0.0)
+find_package(Protobuf 3.0.0)
 find_package(GRPC)
 
 # Message as Protobuf is silent
