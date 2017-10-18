99bb3001b6c9db494809200e2573433491df91e3
Pascal Boeschoten <passiepascal@gmail.com>
2017-10-18 12:52:56 +0200

    Improved MySQL backend

--- src/Backends/MySql/MySqlBackend.cxx
+++ src/Backends/MySql/MySqlBackend.cxx
@@ -4,10 +4,26 @@
 /// \author Pascal Boeschoten (pascal.boeschoten@cern.ch)
 
 #include "MySqlBackend.h"
-#include <boost/algorithm/string.hpp>
 #include <sstream>
+#include <boost/format.hpp>
+#include <boost/algorithm/string.hpp>
+#include <mysql/mysql.h>
 
 using namespace std::string_literals;
+
+
+#define MYSQL_USER "root"
+#define MYSQL_PASS nullptr
+#define MYSQL_UNIX_SOCKET nullptr
+#define MYSQL_CLIENT_FLAG 0
+
+#define MYSQL_DATABASE "o2"
+#define MYSQL_TABLE "configuration"
+#define MYSQL_DATABASE_TABLE MYSQL_DATABASE "." MYSQL_TABLE
+#define MYSQL_KEY "kee"
+#define MYSQL_KEY_LENGTH "512"
+#define MYSQL_VALUE "value"
+
 
 namespace AliceO2
 {
@@ -19,27 +35,115 @@ namespace
 {
 } // Anonymous namespace
 
-struct MySqlBackendPimpl
+struct MySqlHandle
 {
+    MySqlHandle()
+    {
+      handle = mysql_init(nullptr);
+      if (handle == nullptr) {
+        throw std::runtime_error(mysql_error(handle));
+      }
+    }
+
+    ~MySqlHandle()
+    {
+      if (handle != nullptr) {
+        mysql_close(handle);
+      }
+    }
+
+    MYSQL* handle;
+};
+
+class MySqlBackendPimpl
+{
+  public:
+
+    MySqlBackendPimpl(const std::string& host, int port) : handle()
+    {
+      if (mysql_real_connect(getHandle(), host.c_str(), MYSQL_USER, MYSQL_PASS, MYSQL_DATABASE, port,
+          MYSQL_UNIX_SOCKET, MYSQL_CLIENT_FLAG) == 0)
+      {
+        throw std::runtime_error(mysql_error(getHandle()));
+      }
+    }
+
+    ~MySqlBackendPimpl()
+    {
+    }
+
+    MYSQL* getHandle()
+    {
+      return handle.handle;
+    }
+
+  private:
+
+    MySqlHandle handle;
 };
 
 MySqlBackend::MySqlBackend(const std::string& host, int port)
-    : mPimpl(new MySqlBackendPimpl())
+    : mPimpl(new MySqlBackendPimpl(host, port))
 {
+
 }
 
 void MySqlBackend::putString(const std::string& path, const std::string& value)
 {
+  auto query = (boost::format("INSERT INTO " MYSQL_DATABASE_TABLE " VALUES('%1%', '%2%')") % path % value).str();
+  if (mysql_query(mPimpl->getHandle(), query.c_str())) {
+    throw std::runtime_error("MySQL query failed");
+  }
 }
 
 auto MySqlBackend::getString(const std::string& path) -> Optional<std::string>
 {
-  return {};
+  auto query = (boost::format("SELECT " MYSQL_VALUE "FROM " MYSQL_TABLE " WHERE " MYSQL_KEY "='%1%'") % path).str();
+  if (mysql_query(mPimpl->getHandle(), query.c_str())) {
+    throw std::runtime_error("MySQL query failed");
+  }
+
+  // TODO read data
+  MYSQL_RES* result = mysql_store_result(mPimpl->getHandle());
+  if (result == nullptr) {
+    throw std::runtime_error("MySQL result == nullptr");
+  }
+
+  if (mysql_num_fields(result) != 1) {
+    throw std::runtime_error("MySQL returned incorrect number of fields (!=1)");
+  }
+
+  MYSQL_ROW row = mysql_fetch_row(result);
+  return std::string(row[0]);
 }
 
 auto MySqlBackend::getRecursive(const std::string& path) -> Tree::Node
 {
+  auto query = (boost::format(
+      "SELECT " MYSQL_KEY "," MYSQL_VALUE " FROM " MYSQL_TABLE " WHERE " MYSQL_KEY " LIKE '%1%%%'") % path).str();
+
+  if (mysql_query(mPimpl->getHandle(), query.c_str())) {
+    throw std::runtime_error("MySQL query failed");
+  }
+
+  // TODO read data
+  MYSQL_RES* result = mysql_store_result(mPimpl->getHandle());
+  if (result == nullptr) {
+    throw std::runtime_error("MySQL result == nullptr");
+  }
+
+  if (mysql_num_fields(result) != 2) {
+    throw std::runtime_error("MySQL returned incorrect number of fields (!=2)");
+  }
+
   std::vector<std::pair<std::string, Tree::Leaf>> keyValuePairs;
+  {
+    MYSQL_ROW row;
+    while ((row = mysql_fetch_row(result))) {
+      keyValuePairs.emplace_back(row[0], row[1]);
+    }
+  }
+
   return Tree::keyValuesToTree(keyValuePairs);
 }
 
