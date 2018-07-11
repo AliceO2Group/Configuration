c5f0e199a362abfdc5da210435d4912652c2f44f
Adam Wegrzynek <awegrzyn@users.noreply.github.com>
2018-07-11 17:51:29 +0300

    Do not look for libjson11

--- cmake/FindPpconsul.cmake
+++ cmake/FindPpconsul.cmake
@@ -5,28 +5,18 @@
 #  - PPCONSUL_INCLUDE_DIR: include dir
 include(FindPackageHandleStandardArgs)
 
-find_library(_LIBPPCONSUL
+find_library(PPCONSUL_LIBRARIES
              NAMES ppconsul
              HINTS ${PPCONSUL_DIR} ${PPCONSUL_LIBRARY_DIRS}/..
              PATH_SUFFIXES lib)
-find_library(_LIBJSON11
-             NAMES json11
-             HINTS ${PPCONSUL_DIR} ${PPCONSUL_LIBRARY_DIRS}/..
-             PATH_SUFFIXES lib)
+
 find_path(PPCONSUL_INCLUDE_DIR
           NAMES ppconsul/ppconsul.h
           HINTS ${PPCONSUL_DIR} ${PPCONSUL_INCLUDE_DIRS}/..
           PATH_SUFFIXES include)
 
 find_package_handle_standard_args(Ppconsul DEFAULT_MSG
-                                  _LIBPPCONSUL
+                                  PPCONSUL_LIBRARIES
                                   PPCONSUL_INCLUDE_DIR)
 
-set(PPCONSUL_LIBRARIES ${_LIBPPCONSUL})
-if(${_LIBJSON11})
-  set(PPCONSUL_LIBRARIES ${PPCONSUL_LIBRARIES};${_LIBJSON11})
-endif()
 message(STATUS "FOUND PPCONSUL LIBS ==> ${PPCONSUL_LIBRARIES}")
-
-unset(_LIBPPCONSUL)
-unset(_LIBJSON11)
