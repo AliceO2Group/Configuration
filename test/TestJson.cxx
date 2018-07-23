a8868b22d34e4e53ef72cb2fcf0e2dcd3bd89106
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2018-07-23 11:12:43 +0200

    Add recursiveMap to Json backend

--- test/TestJson.cxx
+++ test/TestJson.cxx
@@ -26,7 +26,7 @@ BOOST_AUTO_TEST_CASE(JsonFileTest)
   const std::string TEMP_FILE = "/tmp/alice_o2_configuration_test_file.json";
   {
     std::ofstream stream(TEMP_FILE);
-    stream << R"({"menu": {
+    stream << R"({"configuration_library": {
       "id": "file",
       "popup": {
         "menuitem": {
@@ -38,20 +38,20 @@ BOOST_AUTO_TEST_CASE(JsonFileTest)
 
   auto conf = ConfigurationFactory::getConfiguration("json:/" + TEMP_FILE);
 
-  BOOST_CHECK_EQUAL(conf->get<std::string>("menu/id").get_value_or(""), "file");
-  BOOST_CHECK_EQUAL(conf->get<std::string>("menu/popup/menuitem/one/onclick").get_value_or(""), "CreateNewDoc");
-  BOOST_CHECK_EQUAL(conf->get<int>("menu/popup/menuitem/one/value").get_value_or(0), 123);
+  BOOST_CHECK_EQUAL(conf->get<std::string>("configuration_library/id").get_value_or(""), "file");
+  BOOST_CHECK_EQUAL(conf->get<std::string>("configuration_library/popup/menuitem/one/onclick").get_value_or(""), "CreateNewDoc");
+  BOOST_CHECK_EQUAL(conf->get<int>("configuration_library/popup/menuitem/one/value").get_value_or(0), 123);
 
-  BOOST_CHECK_EQUAL(conf->get<int>("menu/popup/menuitem/one/wrong_key").get_value_or(0), 0);
-  BOOST_CHECK_EQUAL(conf->get<std::string>("menu/popup/menuitem/one/wrong_key_string").get_value_or("string"), "string");
+  BOOST_CHECK_EQUAL(conf->get<int>("configuration_library/popup/menuitem/one/wrong_key").get_value_or(0), 0);
+  BOOST_CHECK_EQUAL(conf->get<std::string>("configuration_library/popup/menuitem/one/wrong_key_string").get_value_or("string"), "string");
 }
 
-BOOST_AUTO_TEST_CASE(Json2FileRecursiveTest)
+BOOST_AUTO_TEST_CASE(JsonFileRecursiveTest)
 {
   const std::string TEMP_FILE = "/tmp/alice_o2_configuration_test_file.json";
   {
     std::ofstream stream(TEMP_FILE);
-    stream << R"({"menu": {
+    stream << R"({"configuration_library": {
       "id": "file",
       "popup": {
         "menuitem": {
@@ -62,9 +62,43 @@ BOOST_AUTO_TEST_CASE(Json2FileRecursiveTest)
   }
 
   auto conf = ConfigurationFactory::getConfiguration("json:/" + TEMP_FILE);
-  auto subTree = conf->getSubTree("menu/popup/menuitem/one");
-  BOOST_CHECK_EQUAL(subTree.get<int>("value"), 123);
-  BOOST_CHECK_EQUAL(subTree.get<std::string>("onclick"), "CreateNewDoc");
+  auto subTree = conf->getSubTree("configuration_library");
+  BOOST_CHECK_EQUAL(subTree.get<std::string>("id"), "file");
+  BOOST_CHECK_EQUAL(subTree.get<int>("popup.menuitem.one.value"), 123);
+  BOOST_CHECK_EQUAL(subTree.get<std::string>("popup.menuitem.one.onclick"), "CreateNewDoc");
+
+  auto leaf = conf->getSubTree("configuration_library/popup/menuitem/one");
+  BOOST_CHECK_EQUAL(leaf.get<int>("value"), 123);
+  BOOST_CHECK_EQUAL(leaf.get<std::string>("onclick"), "CreateNewDoc");
+
+}
+
+BOOST_AUTO_TEST_CASE(JsonFileRecursiveMapTest)
+{
+  const std::string TEMP_FILE = "/tmp/alice_o2_configuration_test_file.json";
+  {
+    std::ofstream stream(TEMP_FILE);
+    stream << R"({"configuration_library": {
+      "id": "file",
+      "popup": {
+        "menuitem": {
+          "one": {"value": "123", "onclick": "CreateNewDoc"}
+        }
+      }
+    }})";
+  }
+
+  auto conf = ConfigurationFactory::getConfiguration("json:/" + TEMP_FILE);
+  auto map = conf->getRecursiveMap("configuration_library");
+  BOOST_CHECK_EQUAL(map.size(), 3);
+  BOOST_CHECK_EQUAL(map["id"], "file");
+  BOOST_CHECK_EQUAL(map["popup/menuitem/one/onclick"], "CreateNewDoc");
+  BOOST_CHECK_EQUAL(map["popup/menuitem/one/value"], "123");
+
+  auto leaf = conf->getRecursiveMap("configuration_library/popup/menuitem/one");
+  BOOST_CHECK_EQUAL(leaf.size(), 2);
+  BOOST_CHECK_EQUAL(leaf["value"], "123");
+  BOOST_CHECK_EQUAL(leaf["onclick"], "CreateNewDoc");
 }
 
 } // Anonymous namespace
