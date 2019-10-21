7bd401a31d7c7dfaf9704f59599c87cc4170a75f
Kostas Alexopoulos <kostorr.alex@gmail.com>
2019-10-21 14:34:49 +0200

    Add boolean getter (#49)
    * Add a boolean getter
    
    * Throw std::runtime_error and not std::string
    
    * clang-format

--- src/Backends/Ini/IniBackend.cxx
+++ src/Backends/Ini/IniBackend.cxx
@@ -9,49 +9,50 @@
 // or submit itself to any jurisdiction.
 
 /// \file IniBackend.cxx
-/// \brief Configuration interface for files. Port of Configuration.h & Configuration.cxx
+/// \brief Configuration interface for files. Port of Configuration.h &
+/// Configuration.cxx
 ///
 /// \author Sylvain Chapeland, CERN
 /// \author Pascal Boeschoten, CERN
 
 #include "IniBackend.h"
-#include <vector>
-#include <boost/property_tree/ini_parser.hpp>
 #include <boost/algorithm/string/predicate.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <vector>
 
-namespace o2
-{
-namespace configuration
-{
-namespace backends
-{
+namespace o2 {
+namespace configuration {
+namespace backends {
 
 /// Load the configuration from given path
 /// \param path  Path to configuration data.
 ///              Example: file:/configDir/example.cfg
 ///              Accepted prefix:
 ///                 file:     for a file accessible from the filesystem
-///              Accepted suffix, to define file format (TODO: separate doc for file formats):
+///              Accepted suffix, to define file format (TODO: separate doc for
+///              file formats):
 ///                 .ini, .cfg    see example.cfg
 /// \exception   Throws a <std::string> exception on error.
-void loadConfigFile(const std::string& filePath, boost::property_tree::ptree& pt)
-{
-  if (filePath.length() == 0) { throw std::runtime_error("Invalid argument"); }
+void loadConfigFile(const std::string &filePath,
+                    boost::property_tree::ptree &pt) {
+  if (filePath.length() == 0) {
+    throw std::runtime_error("Invalid argument");
+  }
 
   // INI file
   for (auto suffix : {".ini", ".cfg"}) {
     if (boost::algorithm::ends_with(filePath, suffix)) {
       try {
         boost::property_tree::ini_parser::read_ini(filePath, pt);
-      }
-      catch (const boost::property_tree::ini_parser::ini_parser_error& perr) {
+      } catch (const boost::property_tree::ini_parser::ini_parser_error &perr) {
         std::stringstream ss;
         if (perr.line()) {
-          ss << perr.message() << " in " << perr.filename() << " line " << perr.line();
+          ss << perr.message() << " in " << perr.filename() << " line "
+             << perr.line();
         } else {
           ss << perr.message() << " " << perr.filename();
         }
-        throw ss.str();
+        throw std::runtime_error(ss.str());
       }
       return;
     }
@@ -59,27 +60,26 @@ void loadConfigFile(const std::string& filePath, boost::property_tree::ptree& pt
   throw std::runtime_error("Invalid type in file name");
 }
 
-IniBackend::IniBackend(const std::string& filePath)
-{
+IniBackend::IniBackend(const std::string &filePath) {
   loadConfigFile(filePath, mPropertyTree);
 }
 
-void IniBackend::putString(const std::string&, const std::string&)
-{
+void IniBackend::putString(const std::string &, const std::string &) {
   throw std::runtime_error("IniBackend does not support putting values");
 }
 
-boost::optional<std::string> IniBackend::getString(const std::string& path)
-{
-  // To use a custom separator instead of the default '.', we need to construct the path_type object explicitly
-  return mPropertyTree.get_optional<std::string>(decltype(mPropertyTree)::path_type(addPrefix(path), getSeparator()));
+boost::optional<std::string> IniBackend::getString(const std::string &path) {
+  // To use a custom separator instead of the default '.', we need to construct
+  // the path_type object explicitly
+  return mPropertyTree.get_optional<std::string>(
+      decltype(mPropertyTree)::path_type(addPrefix(path), getSeparator()));
 }
 
-boost::property_tree::ptree IniBackend::getRecursive(const std::string&/* path*/)
-{
+boost::property_tree::ptree
+IniBackend::getRecursive(const std::string & /* path*/) {
   return mPropertyTree;
 }
 
-} // namespace configuration
 } // namespace backends
+} // namespace configuration
 } // namespace o2
