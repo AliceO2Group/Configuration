ff772d9e413aee489d89f37122cf6ac648d3c628
Adam Wegrzynek <awegrzyn@users.noreply.github.com>
2021-09-13 12:28:02 +0200

    Modify copyright header (#74)

--- src/Backends/Json/JsonBackend.cxx
+++ src/Backends/Json/JsonBackend.cxx
@@ -1,8 +1,9 @@
-// Copyright CERN and copyright holders of ALICE O2. This software is
-// distributed under the terms of the GNU General Public License v3 (GPL
-// Version 3), copied verbatim in the file "COPYING".
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
-// See http://alice-o2.web.cern.ch/license for full licensing information.
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
 //
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
@@ -35,7 +36,7 @@ void JsonBackend::readJsonFile(bool isStream)
 {
   try {
     if (isStream) {
-      std::istringstream ss; 
+      std::istringstream ss;
       ss.str(mPath);
       boost::property_tree::read_json(ss, mTree);
     } else {
--- src/CommandLineUtilities/Convert.cxx
+++ src/CommandLineUtilities/Convert.cxx
@@ -1,8 +1,9 @@
-// Copyright CERN and copyright holders of ALICE O2. This software is
-// distributed under the terms of the GNU General Public License v3 (GPL
-// Version 3), copied verbatim in the file "COPYING".
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
-// See http://alice-o2.web.cern.ch/license for full licensing information.
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
 //
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
@@ -24,7 +25,7 @@ int main(int argc, char *argv[]) {
     ("src", boost::program_options::value<std::string>(&sourceUri)->required(), "Source URI")
     ("dest", boost::program_options::value<std::string>(&destinationUri)->required(), "Destination URI")
   ;
-  
+
   boost::program_options::variables_map vm;
   boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
   boost::program_options::notify(vm);
